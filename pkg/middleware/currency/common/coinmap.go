9b7644cbd478a4738eb7e64e078a5d3d1af5bdca
Zhao KK <kikakkz@hotmailcom>
2022-06-15 03:54:18 +0000

    Add BNB and BUSD support

--- pkg/middleware/currency/common/coinmap.go
+++ pkg/middleware/currency/common/coinmap.go
@@ -2,24 +2,28 @@ package common
 
 func MapCoin(coinName string) string {
 	coinMap := map[string]string{
-		"fil":        "filecoin",
-		"filecoin":   "filecoin",
-		"tfilecoin":  "filecoin",
-		"btc":        "bitcoin",
-		"bitcoin":    "bitcoin",
-		"tbitcoin":   "bitcoin",
-		"tethereum":  "ethereum",
-		"eth":        "ethereum",
-		"ethereum":   "ethereum",
-		"tusdt":      "tether",
-		"usdt":       "tether",
-		"tusdterc20": "tether",
-		"usdterc20":  "tether",
-		"tusdttrc20": "tether",
-		"usdttrc20":  "tether",
-		"sol":        "solana",
-		"solana":     "solana",
-		"tsolana":    "solana",
+		"fil":          "filecoin",
+		"filecoin":     "filecoin",
+		"tfilecoin":    "filecoin",
+		"btc":          "bitcoin",
+		"bitcoin":      "bitcoin",
+		"tbitcoin":     "bitcoin",
+		"tethereum":    "ethereum",
+		"eth":          "ethereum",
+		"ethereum":     "ethereum",
+		"tusdt":        "tether",
+		"usdt":         "tether",
+		"tusdterc20":   "tether",
+		"usdterc20":    "tether",
+		"tusdttrc20":   "tether",
+		"usdttrc20":    "tether",
+		"sol":          "solana",
+		"solana":       "solana",
+		"tsolana":      "solana",
+		"tbinancecoin": "binancecoin",
+		"binancecoin":  "binancecoin",
+		"tbinanceusd":  "binance-usd",
+		"bianaceusd":   "binance-busd",
 	}
 	if coin, ok := coinMap[coinName]; ok {
 		return coin
@@ -29,12 +33,14 @@ func MapCoin(coinName string) string {
 
 func PriceCoin(coinName string) bool {
 	priceMap := map[string]string{
-		"tusdt":      "tether",
-		"usdt":       "tether",
-		"tusdterc20": "tether",
-		"usdterc20":  "tether",
-		"tusdttrc20": "tether",
-		"usdttrc20":  "tether",
+		"tusdt":       "tether",
+		"usdt":        "tether",
+		"tusdterc20":  "tether",
+		"usdterc20":   "tether",
+		"tusdttrc20":  "tether",
+		"usdttrc20":   "tether",
+		"tbinanceusd": "binance-usd",
+		"bianaceusd":  "binance-usd",
 	}
 	_, ok := priceMap[coinName]
 	return ok
